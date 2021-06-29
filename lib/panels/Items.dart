84f3674ac86cd36a5ac65c7e14ae112ed87b16b9
noelli <tobiah@noell.li>
2021-06-29 14:19:48 +0200

    Added Functionality to add Items and display them

--- lib/panels/Items.dart
+++ lib/panels/Items.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:kiosk/common/database.dart';
 import 'package:kiosk/common/models.dart';
+
+bool isNumeric(String s) {
+  return double.tryParse(s) != null;
+}
 
 class ItemsPanel extends StatefulWidget {
   ItemsPanel({
@@ -12,14 +17,31 @@ class ItemsPanel extends StatefulWidget {
 
 class _ItemsPanelState extends State<ItemsPanel> {
   bool _loading = true;
-  bool _viewFAB = false;
+  bool _noItems = false;
+  bool _viewFAB = true;
 
   List<Item> _items = [Item("Test", 0)];
 
+  void _getItems() async {
+    List<Item> _temp = await getItems();
+    if (_temp.isNotEmpty) {
+      setState(() {
+        _loading = false;
+        _noItems = false;
+        _items = _temp;
+      });
+    } else {
+      setState(() {
+        _loading = false;
+        _noItems = true;
+        _items = _temp;
+      });
+    }
+  }
+
   @override
   void initState() {
-    // TODO: implement getting Items
-
+    _getItems();
     super.initState();
   }
 
@@ -31,23 +53,147 @@ class _ItemsPanelState extends State<ItemsPanel> {
           ? Center(
               child: CircularProgressIndicator(),
             )
-          : ListView.builder(
-              itemCount: _items.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                    title: Text(
-                  "${_items[index].name}",
-                ));
-              },
-            ),
+          : _noItems
+              ? Center(
+                  child: Text("Keine Items bisher."),
+                )
+              : ListView.builder(
+                  itemCount: _items.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      trailing: Container(
+                          width: 200,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              TextButton(
+                                child: Text("1x"),
+                                onPressed: () {},
+                                style: ButtonStyle(backgroundColor:
+                                    MaterialStateProperty.resolveWith<Color>(
+                                  (Set<MaterialState> states) {
+                                    if (states
+                                        .contains(MaterialState.pressed)) {
+                                      return Colors.amber.withOpacity(0.5);
+                                    }
+                                    return Colors
+                                        .amber; // Use the component's default.
+                                  },
+                                )),
+                              ),
+                              TextButton(
+                                child: Text("5x"),
+                                onPressed: () {},
+                                style: ButtonStyle(backgroundColor:
+                                    MaterialStateProperty.resolveWith<Color>(
+                                  (Set<MaterialState> states) {
+                                    if (states
+                                        .contains(MaterialState.pressed)) {
+                                      return Colors.amber.withOpacity(0.5);
+                                    }
+                                    return Colors
+                                        .amber; // Use the component's default.
+                                  },
+                                )),
+                              ),
+                              TextButton(
+                                child: Text("10x"),
+                                onPressed: () {},
+                                style: ButtonStyle(backgroundColor:
+                                    MaterialStateProperty.resolveWith<Color>(
+                                  (Set<MaterialState> states) {
+                                    if (states
+                                        .contains(MaterialState.pressed)) {
+                                      return Colors.amber.withOpacity(0.5);
+                                    }
+                                    return Colors
+                                        .amber; // Use the component's default.
+                                  },
+                                )),
+                              ),
+                            ],
+                          )),
+                      title: Text(
+                        "${_items[index].name}",
+                      ),
+                      subtitle: Text("${_items[index].price} €"),
+                    );
+                  },
+                ),
       floatingActionButton: _viewFAB
           ? FloatingActionButton(
-              // TODO: impement adding Items
-              onPressed: () {},
+              onPressed: () => showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AddItemsOverlay())
+                  .then((value) => _getItems()),
               tooltip: 'Add',
               child: Icon(Icons.add),
             )
           : Container(),
+    );
+  }
+}
+
+class AddItemsOverlay extends StatefulWidget {
+  AddItemsOverlay({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _AddItemsOverlayState createState() => _AddItemsOverlayState();
+}
+
+class _AddItemsOverlayState extends State<AddItemsOverlay> {
+  final _itemKey = GlobalKey<FormState>();
+
+  String name = "";
+  double price = 0;
+
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Item hinzufügen"),
+      content: Form(
+        key: _itemKey,
+        child: Column(
+          children: [
+            TextFormField(
+              onSaved: (String? value) {
+                name = value!;
+              },
+              validator: (String? value) {
+                if (value?.isEmpty ?? false) {
+                  return "Bitte Text eingeben";
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              onSaved: (String? value) {
+                price = double.parse(value!);
+              },
+              validator: (String? value) {
+                if (value == null) {
+                  return "Bitte eine Zahl eingeben:";
+                } else {
+                  if (value.isEmpty || !isNumeric(value)) {
+                    return "Bitte eine Zahl eingeben";
+                  }
+                  return null;
+                }
+              },
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (_itemKey.currentState?.validate() ?? false) {
+                    _itemKey.currentState?.save();
+                    insertItem("items", Item(name, price));
+                    Navigator.of(context).pop();
+                  }
+                },
+                child: Text("Speichern"))
+          ],
+        ),
+      ),
     );
   }
 }
